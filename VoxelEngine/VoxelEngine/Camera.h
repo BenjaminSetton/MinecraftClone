6ef9bb29d8e234759dd2389a64fe2e279c58c547
Benjamin-Setton <BSetton@student.fullsail.edu>
2021-07-11 21:13:38 -0400

    Created the camera class along with the view matrix

---
+++ VoxelEngine/VoxelEngine/Camera.h
@@ -0,0 +1,32 @@
+#ifndef _CAMERA_H
+#define _CAMERA_H
+
+#include <DirectXMath.h>
+
+class Camera
+{
+public:
+
+	Camera();
+	~Camera();
+	Camera(const Camera& camera);
+
+	void Render();
+
+	DirectX::XMFLOAT3 GetPosition();
+	void SetPosition(const DirectX::XMFLOAT3 pos);
+
+	DirectX::XMFLOAT3 GetRotation();
+	void SetRotation(const DirectX::XMFLOAT3 rot);
+
+	DirectX::XMMATRIX GetViewMatrix();
+
+private:
+
+	DirectX::XMFLOAT3 m_position;
+	DirectX::XMFLOAT3 m_rotation;
+	DirectX::XMMATRIX m_viewMatrix;
+};
+
+
+#endif
