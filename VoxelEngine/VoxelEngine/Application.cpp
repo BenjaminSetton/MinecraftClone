ff628f8975b030b24a2dceb6a5f7f1b5b1843d32
Benjamin-Setton <BSetton@student.fullsail.edu>
2021-07-12 22:53:07 -0400

    Changed the HWND style and added a border and MAX-MIN-QUIT buttons

--- VoxelEngine/VoxelEngine/Application.cpp
+++ VoxelEngine/VoxelEngine/Application.cpp
@@ -227,7 +227,7 @@ void Application::InitializeWindows(int& screenWidth, int& screenHeight)
 
 	// Create the window with the screen settings and get the handle to it.
 	m_hwnd = CreateWindowEx(WS_EX_APPWINDOW, m_applicationName, m_applicationName,
-		WS_CLIPSIBLINGS | WS_CLIPCHILDREN | WS_POPUP,
+		WS_OVERLAPPEDWINDOW | WS_POPUP,
 		posX, posY, screenWidth, screenHeight, NULL, NULL, m_hinstance, NULL);
 
 	// Bring the window up on the screen and set it as main focus.
